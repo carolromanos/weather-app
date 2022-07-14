941025aad40efe908576f02ec9433c00eadd1a7a
Carolina Romanos <carolina.romanos.roca@gmail.com>
2022-07-14 19:41:54 +0200

    forecast icons

--- src/app.js
+++ src/app.js
@@ -21,28 +21,38 @@ function formatDate(timestamp) {
     let day = days[date.getDay()];
     return `${day} ${hours}:${minutes}`;
   }
+
+  function formatDay(timestamp){
+    let date = new Date(timestamp * 1000)
+    let day = date.getDay()
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+
+  }
   
   function displayForecast(response) {
     console.log(response.data.daily);
     let forecastElement = document.querySelector("#forecast");
+    let forecast = response.data.daily
   
-    let days = ["Thu", "Fri", "Sat", "Sun"];
   
     let forecastHTML = `<div class="row">`;
-    days.forEach(function (day) {
+
+    forecast.forEach(function (forecastDay) {
       forecastHTML =
         forecastHTML +
         `
         <div class="col-2">
-          <div class="weather-forecast-date">${day}</div>
+          <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
           <img
-            src="http://openweathermap.org/img/wn/50d@2x.png"
+            src= http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png
             alt=""
             width="42"
           />
           <div class="weather-forecast-temperatures">
-            <span class="weather-forecast-temperature-max"> 18° </span>
-            <span class="weather-forecast-temperature-min"> 12° </span>
+            <span class="weather-forecast-temperature-max"> ${Math.floor(forecastDay.temp.max)}°  </span>
+            <span class="weather-forecast-temperature-min"> ${Math.floor(forecastDay.temp.min)}° </span>
           </div>
         </div>
     `;
@@ -91,7 +101,7 @@ function formatDate(timestamp) {
     axios.get(apiUrl).then(displayTemperature);
   }
   function getCurrentLocation (event){
-    event.preventDefault();
+    //event.preventDefault();
     navigator.geolocation.getCurrentPosition(searchLocation);
   }
   function searchLocation(position) {
@@ -139,3 +149,5 @@ function formatDate(timestamp) {
 let currentLocationButton = document.querySelector("#current-location-button");
 
 currentLocationButton.addEventListener("click", getCurrentLocation);
+
+search(getCurrentLocation())
